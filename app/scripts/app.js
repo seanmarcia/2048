851d1d366d934735e4a54086a57b4f72dac9013f
Sean Marcia <sean@cs.uoregon.edu>
2014-05-05 14:08:48 -0400

    Remove some of the unneeded yeoman crap.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,3 @@
 'use strict';
 
-angular
-  .module('twentyfourtyeightApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('twentyfourtyeightApp', [])
