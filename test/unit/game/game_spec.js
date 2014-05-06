1b2fc1ad1a1996de1acc2f58877eb68e0ef3c301
Sean Marcia <sean@cs.uoregon.edu>
2014-05-06 19:17:05 -0400

    Added a test unit directory and initial file for game specs.

---
+++ test/unit/game/game_spec.js
@@ -0,0 +1,8 @@
+describe('Game module', function() {
+  describe('GameManager', function() {
+    // Inject the Game module into this test
+    beforeEach(module('Game'));
+
+    // Our tests will go below here
+  });
+});
