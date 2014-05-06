f699b156535f9b560da14dea7b67f93b118c63d3
Sean Marcia <sean@cs.uoregon.edu>
2014-05-06 19:14:38 -0400

    updated the karma configuration file to list which files it will watch and test.

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,11 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      
+    'app/bower_components/angular/angular.js',
+    'app/bower_components/angular-mocks/angular-mocks.js',
+    'app/bower_components/angular-cookies/angular-cookies.js',
+    'app/scripts/**/*.js',
+    'test/unit/**/*.js'      
     ],
 
 
